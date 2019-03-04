4bc21e617dadf2f60715dfe4157634785e4320dd
Anna Kanska <ann.kanska@gmail.com>
2019-03-04 22:38:07 +0100

    project setup and adding main components-  "header", "books" and "genres"

---
+++ src/components/genres.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+class Genres extends Component {}
+
+export default Genres;
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <div className="logo" />
+        <div className="nav-bar">navigation here</div>
+      </div>
+    );
+  }
+}
+
+export default Header;
