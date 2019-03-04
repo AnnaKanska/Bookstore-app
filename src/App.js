21308d6bc7a136ab18ea044f453562ffbb19cab7
Anna Kanska <ann.kanska@gmail.com>
2019-03-04 23:57:04 +0100

    added a genre-add form and functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/header";
+import Genres from "./components/genres";
 
 class App extends Component {
+  state = {
+    genres: []
+  };
+
+  addGenre = genre => {
+    this.setState({
+      genres: [genre, ...this.state.genres]
+    });
+  };
+
   render() {
     return (
       <div>
         <Header />
+        <Genres onSubmit={this.addGenre} />
+        {JSON.stringify(this.state.genres)}
       </div>
     );
   }
--- src/components/genres.js
+++ src/components/genres.js
@@ -1,5 +1,40 @@
 import React, { Component } from "react";
 
-class Genres extends Component {}
+class Genres extends Component {
+  state = {
+    name: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+  submitGenre = event => {
+    event.preventDefault();
+    this.props.onSubmit({
+      name: this.state.name
+    });
+    this.setState({
+      name: ""
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitGenre}>
+          <input
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            placeholder="new genre"
+          />
+          <button onClick={this.submitGenre}>Add</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Genres;
